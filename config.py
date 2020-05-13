403f9f0ba6199536c8c3e0ff8a509a675d57f690
ErmoGarcia <ermiutx@gmail.com>
2020-05-13 02:25:10 +0200

    Added secure configuration for cookies

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
 
-DEBUG = True
-TESTING = True
+DEBUG = False
+TESTING = False
 SECRET_KEY = os.urandom(24)
 SQLALCHEMY_DATABASE_URI = 'sqlite:///game.db'
+SESSION_COOKIE_SECURE = True,
+SESSION_COOKIE_HTTPONLY = True,
+SESSION_COOKIE_SAMESITE = 'Lax',
