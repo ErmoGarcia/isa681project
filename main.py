06620826bae96ba662c2dd227d1b6f017e95a7e8
ErmoGarcia <ermiutx@gmail.com>
2020-05-13 02:57:43 +0200

    Fixed proxy

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 
 from game import create_app
 from game.play import socketio
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = create_app()
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1)
 
 if __name__ == '__main__':
     socketio.run(app)
